051ea0aabe47bd08687245e6767fc9dc595107d7
snpmyn <snpmyn@126.com>
2019-07-30 18:00:48 +0800

    优化

--- app/src/main/java/application/App.java
+++ app/src/main/java/application/App.java
@@ -2,7 +2,6 @@ package application;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
 import com.zsp.amap.BuildConfig;
 import com.zsp.utilone.timber.configure.TimberInitConfigure;
 
@@ -23,12 +22,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
         // Application本已单例
         instance = this;
         // timber
